32abab1aba4e3008f53927ca714ace312bf1f8d2
PickleVirus <gasobolev@gmail.com>
2016-10-28 17:41:36 +0300

    Update views.py

--- KTT/views.py
+++ KTT/views.py
@@ -1,31 +1,28 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
 
-# Create your views here
+class IndexView(generic.ListView):
+    template_name = 'KTT/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'KTT/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'KTT/detail.html'
 
 
-def detail(request, question_id):
-
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'KTT/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'KTT/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'KTT/results.html'
 
 
 def vote(request, question_id):
